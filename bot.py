f218158cfed4d126d9b2312b5b2c2a9018e9f74a
LOZORD <ljrudberg@gmail.com>
2016-04-16 16:15:26 -0500

    Vaporization works

--- bot.py
+++ bot.py
@@ -1,6 +1,7 @@
 #! /usr/bin/env python
 # -*- coding: utf-8 -*-
 
+# vaporize example:
 # expects 5 pictures
 # make clip.mp4 slideshow of 3 sec long shots using image001.jpg, image002.jpg, ...
 # `ffmpeg -framerate 1/3 -i image%03d.jpg -c:v libx264 -pix_fmt yuv420p clip.mp4`
@@ -9,20 +10,47 @@
 
 import tweepy, time, sys
 from secrets import Secrets
-
-print 'BOT STARTED!\n'
+from subprocess import call
+from os import listdir
+from string import Template
 
 def vaporize(image_dir, audio_file='short-macplus.mp3'):
-  pass
+  # first get image type
+  dir_contents = listdir(image_dir)
+  image_type = None
+
+  TYPES = ['jpg', 'jpeg', 'png', 'tiff']
+
+  for filename in dir_contents:
+    for some_type in TYPES:
+      if filename.endswith(some_type):
+        image_type = some_type
+        break
+
+  slideshow_cmd = Template('ffmpeg -framerate 1/3 -i $idr/image%03d.$itp -c:v libx264 -pix_fmt yuv420p $idr/show.mp4')
+  movie_cmd = Template('ffmpeg -i $idr/show.mp4 -i ./short-macplus.mp3 -vcodec copy $idr/final.mp4')
+
+  slideshow_cmd = slideshow_cmd.substitute(idr = image_dir, itp = image_type)
+  movie_cmd = movie_cmd.substitute(idr = image_dir)
+
+  print '\n\n\t' + slideshow_cmd + '\n\n\t' + movie_cmd + '\n\n'
+
+  call(slideshow_cmd.split(' '))
+  call(movie_cmd.split(' '))
+
+  return (image_dir + '/final.mp4')
 
 def process_status(status):
   print status.text
   print status.user.screen_name
   print '\n'
 
-auth = tweepy.OAuthHandler(Secrets['CONSUMER_KEY'], Secrets['CONSUMER_SECRET'])
-auth.set_access_token(Secrets['ACCESS_KEY'], Secrets['ACCESS_SECRET'])
-api = tweepy.API(auth)
+if __name__ == '__main__':
+  print 'BOT STARTED!\n'
 
-for status in tweepy.Cursor(api.mentions_timeline).items():
-  process_status(status)
+  auth = tweepy.OAuthHandler(Secrets['CONSUMER_KEY'], Secrets['CONSUMER_SECRET'])
+  auth.set_access_token(Secrets['ACCESS_KEY'], Secrets['ACCESS_SECRET'])
+  api = tweepy.API(auth)
+
+  for status in tweepy.Cursor(api.mentions_timeline).items():
+    process_status(status)
