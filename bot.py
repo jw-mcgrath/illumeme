6aa68735e787289d80c87db856f065e7a26f037c
LOZORD <ljrudberg@gmail.com>
2016-04-17 00:18:32 -0500

    Merge fixed

--- bot.py
+++ bot.py
@@ -9,8 +9,10 @@ from random import choice as choose
 from set_interval import set_interval
 from urllib import urlretrieve
 from vaporize import vaporize
+from buildsequence import build
 
 IMG_TYPES = ['jpg', 'jpeg', 'png', 'tiff']
+KEYWORDS = ['vapor', 'vape', 'dank', 'meme', 'uncommon']
 
 def get_image_urls(tweet):
   if 'media' not in tweet.entities:
@@ -59,18 +61,34 @@ def process_status(status, responses):
   print '\t' + status.text
   print '\t' + uname
 
+  # TODO: remove lozord checks
+  '''
   if uname == 'L0Z0RD' and 'test video' in status.text:
     print 'VAPORIZE TEST!'
     result_file_name = vaporize('pics', USE_MP4, IMG_TYPES)
     api.update_with_media(result_file_name, tag_reply(uname, 'ＩＴ ＩＳ ＣＯＭＰＬＥＴＥ'), status.id)
     api.create_favorite(status.id)
-  # reply with the phrase is image is empty
-  elif image_urls == []:
-    resp = tag_reply(uname, choose(responses))
-    print '\t' + resp
-    api.update_status(resp, status.id)
+  '''
+  # reply with the phrase is image is empty and no kws
+  if image_urls == []:
+
+    if status.text in KEYWORDS:
+      # pull a photo from reddit
+      dir_path = './img-%s' % status.id
+      mkdir(dir_path)
+      build(dir_path)
+      # make gif
+      result_path = vaporize(dir_path, False, IMG_TYPES)
+      resp = tag_reply(uname, '4 u, fam #uncommonhacks')
+      api.update_with_media(result_path, resp, status.id)
+    else:
+      resp = tag_reply(uname, choose(responses))
+      print '\t' + resp
+      api.update_status(resp, status.id)
+
     api.create_favorite(status.id)
-  elif uname == 'L0Z0RD' and len(image_urls) > 0:
+
+  else:
     for url in image_urls:
       prepare_image(url, status.id)
       # TODO: find_illuminati...
