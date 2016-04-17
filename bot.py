510c51877774d0ff2ac7a3095a5f6ed8d01dc73d
LOZORD <ljrudberg@gmail.com>
2016-04-16 21:49:26 -0500

    Moved comment; quieted ffmpeg

--- bot.py
+++ bot.py
@@ -2,13 +2,6 @@
 # encoding=utf8
 # -*- coding: utf-8 -*-
 
-# vaporize example:
-# expects 5 pictures
-# make clip.mp4 slideshow of 3 sec long shots using image001.jpg, image002.jpg, ...
-# `ffmpeg -framerate 1/3 -i image%03d.jpg -c:v libx264 -pix_fmt yuv420p clip.mp4`
-# export slideshow to final.mp4 video using short-macplus.mp3 (15 sec long)
-# `ffmpeg -i clip.mp4 -i short-macplus.mp3 -vcodec copy final.mp4`
-
 import tweepy, time, sys
 from secrets import Secrets
 from os import mkdir
--- vaporize.py
+++ vaporize.py
@@ -2,6 +2,13 @@ from os import listdir
 from string import Template
 from subprocess import call
 
+# vaporize example:
+# expects 5 pictures
+# make clip.mp4 slideshow of 3 sec long shots using image001.jpg, image002.jpg, ...
+# `ffmpeg -framerate 1/3 -i image%03d.jpg -c:v libx264 -pix_fmt yuv420p clip.mp4`
+# export slideshow to final.mp4 video using short-macplus.mp3 (15 sec long)
+# `ffmpeg -i clip.mp4 -i short-macplus.mp3 -vcodec copy final.mp4`
+
 def vaporize(image_dir, make_mp4_instead_of_gif, img_types, audio_file='short-macplus.mp3'):
   # first get image type
   dir_contents = listdir(image_dir)
@@ -17,7 +24,7 @@ def vaporize(image_dir, make_mp4_instead_of_gif, img_types, audio_file='short-ma
     print 'UNKNOWN IMAGE TYPE!'
     return None
 
-  slideshow_cmd = Template('ffmpeg -framerate 1/3 -i $idr/image%03d.$itp -c:v libx264 -pix_fmt yuv420p $idr/show.mp4')
+  slideshow_cmd = Template('ffmpeg -loglevel panic -framerate 1/3 -i $idr/image%03d.$itp -c:v libx264 -pix_fmt yuv420p $idr/show.mp4')
   # TODO: pipe `yes` for overwriting?
   slideshow_cmd = '' + slideshow_cmd.substitute(idr = image_dir, itp = image_type)
 
@@ -25,10 +32,10 @@ def vaporize(image_dir, make_mp4_instead_of_gif, img_types, audio_file='short-ma
   result_path = None
 
   if make_mp4_instead_of_gif:
-    result_cmd = Template('ffmpeg -i $idr/show.mp4 -i ./short-macplus.mp3 -vcodec copy $idr/final.mp4')
+    result_cmd = Template('ffmpeg -loglevel panic -i $idr/show.mp4 -i ./short-macplus.mp3 -vcodec copy $idr/final.mp4')
     result_path = image_dir + '/final.mp4'
   else:
-    result_cmd = Template('ffmpeg -i $idr/show.mp4 $idr/final.gif')
+    result_cmd = Template('ffmpeg -loglevel panic -i $idr/show.mp4 $idr/final.gif')
     result_path = image_dir + '/final.gif'
 
   # TODO: pipe `yes` for overwriting?
